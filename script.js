b6efc79c0978e4c34569fc89422beb1d9ce55aeb
Mademlis Athanasios <athamade1@gmail.com>
2024-08-05 14:22:36 +0300

    created project

---
+++ script.js
@@ -0,0 +1,89 @@
+// let h3PointsEl = document.querySelector('.h3-dice-points');
+// let imgDice1El = document.querySelector('.img-dice-1');
+// let imgDice2El = document.querySelector('.img-dice-2');
+// let btnRollDiceEl = document.querySelector('.btn-roll-the-dice');
+// let btnClearEl = document.querySelector('.btn-clear');
+
+
+// init();
+
+
+
+
+
+
+
+
+
+
+
+
+// function init(){
+//   h3PointsEl.textContent = 1;
+//   imgDice1El.src = 'images/dice-1.png';
+//   imgDice2El.src = 'images/dice-red-1.png';
+// }
+
+
+// btnRollDiceEl.addEventListener('click', function(){
+//   let dice1Number = Math.trunc(Math.random() * 6) + 1;
+//   let dice2Number = Math.trunc(Math.random() * 6) + 1;
+
+//   h3PointsEl.textContent = dice1Number + dice2Number;
+
+//   console.log(`button clicked: dice1 number: ${dice1Number}`);
+//   console.log(`button clicked: dice2 number: ${dice2Number}`);
+
+//   switch (dice1Number){
+//     case 1:
+//       imgDice1El.src = 'images/dice-1.png';
+//       break;
+//     case 2:
+//       imgDice1El.src = 'images/dice-2.png';
+//       break;
+//     case 3:
+//       imgDice1El.src = 'images/dice-3.png';
+//       break;
+//     case 4:
+//       imgDice1El.src = 'images/dice-4.png';
+//       break;
+//     case 5:
+//       imgDice1El.src = 'images/dice-5.png';
+//       break;
+//     case 6:
+//       imgDice1El.src = 'images/dice-6.png';
+//       break;
+//   }
+
+//   switch (dice2Number){
+//     case 1:
+//       imgDice2El.src = 'images/dice-red-1.png';
+//       break;
+//     case 2:
+//       imgDice2El.src = 'images/dice-red-2.png';
+//       break;
+//     case 3:
+//       imgDice2El.src = 'images/dice-red-3.png';
+//       break;
+//     case 4:
+//       imgDice2El.src = 'images/dice-red-4.png';
+//       break;
+//     case 5:
+//       imgDice2El.src = 'images/dice-red-5.png';
+//       break;
+//     case 6:
+//       imgDice2El.src = 'images/dice-red-6.png';
+//       break;
+//   }
+
+// });
+
+
+
+// btnClearEl.addEventListener('click', init());
+
+
+// // btnClearEl.addEventListener('click', function(){
+// //   console.log('clear button clicked');
+// //   init();
+// // });
---
+++ scripts/tavli.js
@@ -0,0 +1,83 @@
+let h3DiceScoreEl = document.querySelector('.h3-dice-score');
+let imgDice1El  = document.querySelector('.img-dice-1');
+let imgDice2El  = document.querySelector('.img-dice-2');
+let btnRollDiceEl  = document.querySelector('.btn-roll-dice');
+let btnClearEl  = document.querySelector('.btn-clear');
+
+
+init();
+
+
+
+
+
+
+btnRollDiceEl.addEventListener('click', function(){
+    console.log('btnRollDice Clicked');
+    let dice1 = Math.trunc(Math.random() * 6) + 1;
+    let dice2 = Math.trunc(Math.random() * 6) + 1;
+    console.log(`dice 1 : ${dice1} / dice 2 : ${dice2} // sum = ${dice1+dice2}`);
+    h3DiceScoreEl.textContent = dice1 + dice2;
+
+    rollTheDice(dice1,dice2);
+    
+});
+
+btnClearEl.addEventListener('click', function(){
+    console.log('btn clear clicked')
+    init();
+});
+
+
+function rollTheDice(dice1, dice2){
+    console.log('rollthedice function run successfully')
+    switch (dice1){
+        case 1:
+            imgDice1El.src = '../images/dice-1.png';
+            break;
+        case 2:
+            imgDice1El.src = '../images/dice-2.png';
+            break;
+        case 3:
+            imgDice1El.src = '../images/dice-3.png';
+            break;
+        case 4:
+            imgDice1El.src = '../images/dice-4.png';
+            break;
+        case 5:
+            imgDice1El.src = '../images/dice-5.png';
+            break;
+        case 6:
+            imgDice1El.src = '../images/dice-6.png';
+            break;
+    }
+
+    switch (dice2){
+        case 1:
+            imgDice2El.src = '../images/dice-1.png';
+            break;
+        case 2:
+            imgDice2El.src = '../images/dice-2.png';
+            break;
+        case 3:
+            imgDice2El.src = '../images/dice-3.png';
+            break;
+        case 4:
+            imgDice2El.src = '../images/dice-4.png';
+            break;
+        case 5:
+            imgDice2El.src = '../images/dice-5.png';
+            break;
+        case 6:
+            imgDice2El.src = '../images/dice-6.png';
+            break;
+    }
+
+}
+
+function init(){
+    console.log('init() function run successfully');
+    h3DiceScoreEl.textContent = 'Ρίξε τα ζάρια';
+    imgDice1El.src = '../images/dice-1.png';
+    imgDice2El.src = '../images/dice-1.png';
+}
