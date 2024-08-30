26d45e9223e22d578c4499439f23c150d37ad2e5
Mademlis Athanasios <athamade1@gmail.com>
2024-08-30 12:21:43 +0300

    sound button FINALE

--- scripts/catan.js
+++ scripts/catan.js
@@ -16,27 +16,19 @@ let closePopupEl = document.querySelector('.close-btn');
 
 let diceHistory = [];
 var pirate=0;
-<<<<<<< HEAD
-let flag=false;
-=======
-var flag=0; //flag = 1 symainei mute sound
->>>>>>> 79d451fb24e8fb3f16c1317f540d112978838586
+let flag=false;//flag = 1 symainei mute sound
 
 
 
 //----------ixos gia zaria starts here----------------
+
 function playMusic1(){
 
     var music1 = new Audio('../sound/sound_check.mp3');
-<<<<<<< HEAD
     
     
     
     if (flag){
-=======
-
-    if (flag==1){
->>>>>>> 79d451fb24e8fb3f16c1317f540d112978838586
         music1.pause();
         console.log('dice sound OFF');
     }
@@ -46,31 +38,19 @@ function playMusic1(){
     }
 
 }
-<<<<<<< HEAD
-btnSoundEl.addEventListener('click',function(){
-    flag=!flag;
-    console.log('sound pressed');})
     
-=======
 //----------ixos gia zaria ends here----------------
->>>>>>> 79d451fb24e8fb3f16c1317f540d112978838586
 
 //----------koumpi gia anoigma kleisimo ixou starts here--------
 btnSoundEl.addEventListener('click',function(){
-    if (flag==0){
-        flag=1;
-        console.log("btn sound clicked / sound OFF");
-    } else {
-        flag=0;
-        console.log("btn sound clicked / sound ON");
-    }
-})
+    this.classList.toggle('muted');
+    flag=!flag;
+    console.log('sound pressed');})
 //----------koumpi gia anoigma kleisimo ixou ends here--------
 
 //----------ixos gia peirates starts here----------------
 function playMusic2(){
     var music2 = new Audio('../sound/7pirates.wav');
-<<<<<<< HEAD
     if (flag){
         music2.pause();
         
@@ -79,17 +59,7 @@ function playMusic2(){
         music2.play();
 
     }
-=======
-    // music2.play();
-    if (flag==1){
-        music2.pause();
-        console.log('pirates sound OFF');
->>>>>>> 79d451fb24e8fb3f16c1317f540d112978838586
-    }
-    else{
-        music2.play();
-        console.log('pirates sound ON');
-    }
+    
 }
 //----------ixos gia peirates ends here----------------
 
@@ -236,14 +206,12 @@ function rollTheDice(dice1, dice2,dice3){
       } 
       if (pirate===7){
         playMusic2();
-<<<<<<< HEAD
         //flag=0;
-        init();
-=======
-        // flag=0;
         // init();
-        pirate=0;  //prepei na allaksei se synarthsh
->>>>>>> 79d451fb24e8fb3f16c1317f540d112978838586
+        // flag=0;
+        
+        initAfterPirates();
+        
         document.querySelector('.container').style.backgroundColor='red';
 
       }
@@ -258,8 +226,19 @@ function init(){
     scorepiratesEl.textContent=0;
     pirate=0;
     
-    
     imgDice1El.src = '../images/dice-1.png';
     imgDice2El.src = '../images/dice-red-1.png';
     imgDice3El.src = "../images/pirate.png";
+
+    const soundButton = document.getElementById('soundButton');
+    soundButton.classList.remove('muted'); // Remove the muted class to reset color
+}
+
+function initAfterPirates(){
+    console.log('initAfterPirates() function run successfully');
+    document.querySelector('.container').style.backgroundColor='transparent';
+    scorepiratesEl.textContent=0;
+    
+    pirate=0;
+    
 }
